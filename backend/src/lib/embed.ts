f519f86c42c0122862f412cccf018e1c85cda896
huyvu38 <vu38@purdue.edu>
2025-07-22 01:21:23 +0700

    Update info
    Signed-off-by: huyvu38 <vu38@purdue.edu>

--- backend/src/lib/embed.ts
+++ backend/src/lib/embed.ts
@@ -5,16 +5,19 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const resumeChunks = [
-    "Huy Vu is a Computer Science and Data Science undergraduate at Purdue University from August 2023 to May 2027",
+    "Huy Vu is a Computer Science undergraduate at Purdue University from August 2023 to ",
     "GPA is 3.6/4.0",
     "Awards: Dean’s List & Semester Honors in 2023 and 2024",
     "Relevant coursework: Object-Oriented Programming, Discrete Mathematics, Linear Algebra, Data Structures & Algorithms, Computer Architecture, Systems Programming",
-    "Worked as a software engineer intern at Hyperlogy in 2024 in Hanoi",
-    "Worked as a data science researcher at Caterpillar.Inc in West Lafayette",
     "Working as a teaching assistant for Computer Science course at Purdue University (Helping more than 400 students and holding 2 lab sessions each semester)",
-    "Built RAG chatbot using OpenAI, integrated with Node.js, Express.js, JavaScript, HTML/CSS, OpenAI API",
-    "Built a fullstack web app for emotional support: journaling, mood tracking, reflection, music with Spotify API",
-    "Skills: JavaScript, Python, HTML/CSS, SQL, C/C++",
+    "Worked as a researcher assistant for VIPER Lab at Purdue University from ",
+    "Worked as a software engineer intern at Hyperlogy from May to July 2024 in Hanoi",
+    "Worked as a data science researcher at Caterpillar.Inc from August 2023 to May 2024 in West Lafayette",
+    "Built RAG chatbot using OpenAI's text embedding, integrated with Node.js, Express.js, JavaScript, HTML/CSS, Pinecone, Prisma, MongoDB",
+    "Built a fullstack web app for emotional support: journaling, mood tracking, reflection, music suggestion with Spotify API",
+    "Programming languages: Java, Python, C/C++, JavaScript, TypeScript, HTML/CSS, SQL, R",
+    "Frameworks and libraries:  React, Node.js, Express.js, Next.js, REST API, Flask, Firebase, OpenCV, GraphQL",
+    "Tools: Git, Github, IntelliJ, VS Code, Jupyter Notebook, Android Studio, Linux, AWS, MongoDB, Docker, Azure",
 ];
 
 const openai = new OpenAI({

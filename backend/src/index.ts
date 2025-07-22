08684a8a79d61df645c7ea98011390738177a81a
huyvu38 <vu38@purdue.edu>
2025-07-22 16:02:59 +0700

    Fix port
    Signed-off-by: huyvu38 <vu38@purdue.edu>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import {askResume} from "./lib/rag";
 dotenv.config();
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
